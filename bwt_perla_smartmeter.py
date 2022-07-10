bd1b6479c38f95c71aeedffc9abc4fa89ab58dc6
drohhyn <drohhyn@users.noreply.github.com>
2022-07-10 13:20:39 +0200

    Include NaCl (Regeneriermittel) and function for capture + send as variables

--- bwt_perla_smartmeter.py
+++ bwt_perla_smartmeter.py
@@ -14,8 +14,7 @@ import paho.mqtt.client as mqtt
 bwt_ipaddress = "192.168.0.mmm"
 bwt_password="passwd"
 mqtt_address="192.168.0.nnn"
-mqtt_topic_throughput="water/bwt/durchfluss"
-mqtt_topic_volume="water/bwt/volumen"
+mqtt_topic="water/bwt/"
 
 #def bwt_connect():
 vncclient = api.connect(bwt_ipaddress, password=None)
@@ -40,6 +39,30 @@ def bwt_login():
     vncclient.mouseDown(1)
     vncclient.mouseUp(1)
 
+def send_capture(var_name, var_name_old, regex_exp, x_pos, y_pos, x_size, y_size):
+    vncclient.captureRegion(var_name+'.png',x_pos,y_pos,x_size,y_size)
+    output_file=pytesseract.image_to_string(Image.open(var_name+'.png'),lang = 'eng',config = '-c page_separator=""')
+    #print(output_file)
+    output_regex=re.search('(.*)'+regex_exp,output_file)
+    #print(var_name+" output_regex:",output_regex)
+    if output_regex:
+        output_regex=output_regex.group(1)
+        output_regex=output_regex.strip()
+        if output_regex=="O":
+            output_regex=0
+        #print(var_name+" output:",output_regex)
+    else:
+        print('OCR '+var_name+' failed')
+        print('Trying to re-login')
+        bwt_login()
+    if output_regex!=var_name_old:
+        #print("MQTT: Publish throughput: ",throughput)
+        try:
+            mqttclient.publish(mqtt_topic + var_name, payload=output_regex, qos=1, retain=False)
+        except:
+            print("MQTT: Publish throughput failed!")
+        var_name_old=output_regex
+
 bwt_login()
 
 mqttclient=mqtt.Client()
@@ -48,70 +71,14 @@ mqttclient.loop_start()
 mqttclient.reconnect_delay_set(min_delay=1, max_delay=120)
 throughput_old=-1
 volume_old=-1
+NaCl_old=-1
 while True:
     # Capture regions
-    vncclient.captureRegion('throughput.png',50,70,80,25)
-    vncclient.captureRegion('volume.png',60,150,80,25)
-    throughputocroutput=pytesseract.image_to_string(Image.open('throughput.png'),lang = 'eng',config = '-c page_separator=""')
-    volumeocroutput=pytesseract.image_to_string(Image.open('volume.png'),lang = 'eng',config = '-c page_separator=""')
-    #print("=============================")
-    #print("OCR result troughput: ",throughputocroutput.strip())
-    #print("OCR result volume: ",volumeocroutput.strip())
-    ### Throughput
-    #throughput=re.search('(.*)\|*[MIl1\|]/h', throughputocroutput)
-    throughput=re.search('(.*)\|*./h', throughputocroutput)
-    #print("Throughput regex:",throughput)
-    if throughput:
-        throughput=throughput.group(1)
-        throughput=throughput.strip()
-        if throughput=="O":
-            throughput=0
-        #print("Throughput:",throughput)
-    else:
-        print('OCR throughput failed')
-        print('-----------------')
-        print(throughputocroutput)
-        print('-----------------')
-        print(throughput)
-        print('-----------------')
-        print('Trying to re-login')
-        bwt_login()
-        print('-----------------')
-    ### Volume
-    volume=re.search('(.*)[Il1\|]', volumeocroutput)
-    #print("Volume regex:",volume)
-    if volume:
-        volume=volume.group(1)
-        volume=volume.strip()
-        if volume=="O":
-            volume=0
-        #print("Volume:",volume)
-    else:
-        print('OCR Volume failed')
-        print('-----------------')
-        print(volumeocroutput)
-        print('-----------------')
-        print(volume)
-        print('-----------------')
+    send_capture("throughput", throughput_old, "\|*./h", 50, 70, 80, 25)
+    send_capture("volume", volume_old, "[Il1\|]", 60, 150, 80, 25)
+    send_capture("NaCl", NaCl_old, "%", 198, 108, 45, 25)
+    
     # Keep VNC connection alive
     vncclient.mouseMove(400,0)
     vncclient.mouseDown(1)
     vncclient.mouseUp(1)
-    if throughput!=throughput_old:
-        #print("MQTT: Publish throughput: ",throughput)
-        try:
-            mqttclient.publish(mqtt_topic_throughput, payload=throughput, qos=1, retain=False)
-        except:
-            print("MQTT: Publish throughput failed!")
-        throughput_old=throughput
-    if volume!=volume_old:
-        #print("MQTT: Publish volume:    ",volume)
-        try:
-            mqttclient.publish(mqtt_topic_volume, payload=volume, qos=1, retain=False)
-        except:
-            print("MQTT: Publish volume failed!")
-        volume_old=volume
-
-
-
-
